a5b73140b92c64969468eed1b8cb709a96a3d9d5
Joshua Katz <gravypod@gravypod.com>
2017-01-07 16:25:49 -0500

    Remove summary generation as it's unused.
    It's still in the history if I feal really bored and creative one
    day in the future.

--- list.php
+++ list.php
@@ -1,7 +1,5 @@
 <?php
 
-	require_once("inc/summarizer.class.php");
-
 	// Seconds * Minutes * Hours
 	define("MAX_CACHE_LIFE", 60 * 60 * 1);
 	define("CACHE_FILE", "inc/cache/posts.json");
@@ -12,31 +10,12 @@
 		die(file_get_contents(CACHE_FILE)); // Read and die from that file
 
 	class Post implements JsonSerializable {
-		private $summary;
 		private $date;
 		private $title;
 		private $location;
 		private $catagory;
 
 		public function __construct($post_file_name) {
-			// Generate our summary
-			$summarizer = new Summarizer();
-
-			// Get document corpus
-			$contents = file_get_contents($post_file_name);
-
-			// Clean and remove unneeded items
-			$contents = preg_replace("/(\[[0-9]+\])/", ' ', $contents); // Removes citations boxes ex: [0]
-			$contents = preg_replace("/[^\n\.A-Za-z0-9 ]/", '', $contents); // Non-terminal characters (keeps A-z 0-9 \n and ' ')
-			$contents = preg_replace("/[a-zA-Z]*[:\/\/]*[A-Za-z0-9\-_]+\.+[A-Za-z0-9\.\/%&=\?\-_]+/i", "", $contents); // URLs
-
-			// Summarize
-			$this->summary = $summarizer->get_summary($contents);
-
-			// Remove unwanted spaces.
-			$this->summary = trim(preg_replace('/\s\s+/', ' ', str_replace("\n", " ", $this->summary)));
-
-
 			// Example file name: 02-02-2002_example_file_name.md
 			// basename($name, ".md") => 02-02-2002_example_file_name
 			$post_basename = basename($post_file_name, ".md");
@@ -58,7 +37,6 @@
 			return array(
 				"title" => $this->title,
 				"date" => $this->date,
-				"summary" => $this->summary,
 				"location" => $this->location,
 				"catagory" => $this->catagory
 			);
